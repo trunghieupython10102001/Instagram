d1de0ea5bd05c8d66e1cf743af942856009f4e1a
Phí Trung Hiếu <phitrunghieu@MacBook-Pro-cua-Phi.local>
2021-12-23 01:29:45 +0700

    add constant file

---
+++ instagram-client/src/config/constant.js
@@ -0,0 +1,7 @@
+export const API_KEY = "AIzaSyD4DKkcfD74dfilrVWaqtIzLeaPk3rTKME"
+export const AUTH_DOMAIN = "instagram-ad6aa.firebaseapp.com"
+export const PROJECT_ID = "instagram-ad6aa"
+export const STORAGE_BUCKET = "instagram-ad6aa.appspot.com"
+export const MESSAGING_SENDER_ID = "102497679414"
+export const APP_ID = "1:102497679414:web:127e2087f87b6b2a954097"
+export const MEASUREMENT_ID = "G-M3LYH8E9LV"
