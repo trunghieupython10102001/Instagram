475d851aad17afdbbad447b7889ef1425fa9053c
Phí Trung Hiếu <phitrunghieu@MacBook-Pro-cua-Phi.local>
2021-12-23 09:38:58 +0700

    fix temporary avatar image

--- instagram-client/src/components/Person/Person.jsx
+++ instagram-client/src/components/Person/Person.jsx
@@ -4,7 +4,7 @@ const Person = ({name}) => {
 	return (
 		<div className='Person'>
 			<div className="avatar">
-				<img src="https://scontent.fhan5-6.fna.fbcdn.net/v/t39.30808-6/254549628_1532690087092725_503017275021991785_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=ILiuaQl_WPoAX9OE-EY&_nc_ht=scontent.fhan5-6.fna&oh=00_AT8wprRlFwaV03ty4yeaNh7MesLaxDTqkZIwvEO88T2Rjw&oe=61C3633B" alt="" />
+				<img src="https://firebasestorage.googleapis.com/v0/b/instagram-ad6aa.appspot.com/o/Thomas.jpeg?alt=media&token=09c40ffd-5046-424f-935e-f01b9f676ab7" alt="" />
 			</div>
 			<div className="person-text">
 				<p className="name"><a href="/">{name}</a></p>
--- instagram-client/src/components/Post/Post.jsx
+++ instagram-client/src/components/Post/Post.jsx
@@ -67,7 +67,7 @@ const Post = ({ images, avatar, username, content, location }) => {
 						<div className="avatar">
 							<a href="/">
 								<img
-									src="https://scontent.fhan5-6.fna.fbcdn.net/v/t39.30808-6/254549628_1532690087092725_503017275021991785_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=ILiuaQl_WPoAX9OE-EY&_nc_ht=scontent.fhan5-6.fna&oh=00_AT8wprRlFwaV03ty4yeaNh7MesLaxDTqkZIwvEO88T2Rjw&oe=61C3633B"
+									src="https://firebasestorage.googleapis.com/v0/b/instagram-ad6aa.appspot.com/o/Thomas.jpeg?alt=media&token=09c40ffd-5046-424f-935e-f01b9f676ab7"
 									alt="lalisa"
 								/>
 							</a>
